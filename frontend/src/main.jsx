9bb592b55261e2f151e31bfe5ca941fa44bf15be
Sixtails1 <103711641+Sixtails1@users.noreply.github.com>
2024-10-31 22:10:18 -0600

    Fixed paths for App.jsx.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Home from "./routes/Home";
+import Calendar from "./routes/Calendar";
+import Dashboard from "./routes/Dashboard";
+import Login from "./routes/Login";
+import Logout from "./routes/Logout";
+import Profile from "./routes/Profile";
+import Register from "./routes/Register";
+import NotFound from "./routes/NotFound";
 import reportWebVitals from "./reportWebVitals.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -30,6 +38,10 @@ const router = createBrowserRouter([
       {
         path: "logout",
         element: <Logout />,
+      },
+      {
+        path: "Profile",
+        element: <Profile />,
       },
       {
         path: "register",
